fa708b0674f6e0d9f35b9a5351a8df6abccd419a
guillaume cromer <guillaumecromer@gmail.com>
2019-08-13 16:42:26 +0200

    rails task manager a la mano

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #List
+  get 'tasks', to: 'tasks#index'
+  #Create
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create', as: 'create'
+  #Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update', as: 'task'
+  #Show
+  get 'tasks/:id', to: 'tasks#show', as: 'show'
+  #Delete
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy'
 end
---
+++ db/migrate/20190813095627_change_completed_to_default_in_tasks.rb
@@ -0,0 +1,5 @@
+class ChangeCompletedToDefaultInTasks < ActiveRecord::Migration[5.2]
+  def change
+    change_column :tasks, :completed, :boolean, default: false
+  end
+end
